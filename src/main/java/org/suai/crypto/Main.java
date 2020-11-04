4fd92f66cbfe5bfbf230c85f1cfbdf032a675b98
Konstantin Stepanov <k.stepanov98@yandex.ru>
2020-11-05 02:41:04 +0300

    remove unused imports

--- src/main/java/org/suai/crypto/Main.java
+++ src/main/java/org/suai/crypto/Main.java
@@ -2,12 +2,8 @@ package org.suai.crypto;
 
 import org.suai.crypto.analysis.LinearCryptAnalyzer;
 import org.suai.crypto.spn.SubstitutionPermutationNetwork;
-import org.suai.crypto.util.EquationElement;
-import org.suai.crypto.util.EquationElementType;
-import org.suai.crypto.util.LinearApproximation;
 
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
